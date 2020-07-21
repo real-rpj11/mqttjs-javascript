4a9e3401505b81cf9e939a11248112023b7fbcde
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-07-21 23:09:38 +0800

    Used mqtt.eclipse.org/mqtt as broker host and 443 as broker port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 console.log("index.js");
 
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
+// var client  = mqtt.connect({ host:'test.mosquitto.org', port: 8081})
+var client  = mqtt.connect({ host:'mqtt.eclipse.org/mqtt', port: 443})
 
 client.on('connect', function () {
+    console.log('connected')
   client.subscribe('presence', function (err) {
     if (!err) {
       client.publish('presence', 'Hello mqtt')
@@ -14,5 +15,5 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString())
-  client.end()
+//   client.end()
 })
