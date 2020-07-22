5a40e8a8b46023e48bf016b63d4f0e90cb2197c2
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-07-23 07:12:12 +0800

    Implemented the other formatting for the broker address

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 console.log("index.js");
 
 // var client  = mqtt.connect({ host:'test.mosquitto.org', port: 8081})
-var client  = mqtt.connect({ host:'mqtt.eclipse.org/mqtt', port: 443})
+// or
+// var client  = mqtt.connect('wss://test.mosquitto.org:8081/mqtt')
+
+// var client  = mqtt.connect({ host:'mqtt.eclipse.org/mqtt', port: 443})
+// or
+var client  = mqtt.connect('wss://mqtt.eclipse.org:443/mqtt')
 
 client.on('connect', function () {
     console.log('connected')
